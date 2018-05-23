d2347ad538b0b739fce820cf952d2e9cac451ad6
DESKTOP-05M5JQR\haget <hageteruneko@yahoo.co.jp>
2018-05-23 19:53:56 +0900

    ノーツの動きを制御

---
+++ script/gamegamen/NodeMove.cs
@@ -0,0 +1,33 @@
+﻿//ノーツ動かすやつ
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NodeMove : MonoBehaviour
+{
+    public float moveTime;
+    public GameObject destination;
+    public GameObject lostPosition;
+    private new Transform transform;
+    private float time = 0f;
+    private Vector3 v_start;
+    private Vector3 v_destination;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        v_destination = lostPosition.transform.position;
+        v_start = this.transform.position;
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        var v = time / moveTime;
+        transform.position = Vector3.Lerp(v_start, v_destination, v);
+        time += Time.deltaTime;
+    }
+}
+
