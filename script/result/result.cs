36e5de8165d6614e7b2926590607f840afa0e70f
DESKTOP-05M5JQR\haget <hageteruneko@yahoo.co.jp>
2018-05-23 19:59:32 +0900

    result　表示部分

---
+++ script/result/result.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class result : MonoBehaviour {
+    public Text scoreText;
+    public Text PerfectText;
+    public Text GoodText;
+    public Text BadText;
+    // Use this for initialization
+    void Start () {
+        int resultscore = ScoreCounter.getscore();
+        int resultPerfect = ScoreCounter.getpsum();
+        int resultGood = ScoreCounter.getgsum();
+        int resultBad = ScoreCounter.getbsum();
+
+        scoreText.text = "Score: " + resultscore.ToString(); ;
+        PerfectText.text = "Perfect: " + resultPerfect.ToString(); ;
+        GoodText.text = "Good: "+ resultGood.ToString();
+        BadText.text = "Bad: " + resultBad.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
